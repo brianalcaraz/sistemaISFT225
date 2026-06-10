ad3c356b76e958feb52f8731a85c7fb1c7b02f9c
brianalcaraz <brianalcaraz@isft225.edu.ar>
2026-06-09 21:23:27 -0300

    solucionado ultimo probnlema de case sensitive

--- routes/alumnoRoutes.js
+++ routes/alumnoRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as alumnoController from '../controllers/alumnoController.js';
+import * as alumnoController from '../Controllers/alumnoController.js';
 
 const router = express.Router();
 
